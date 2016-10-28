7cb8807072372d1ee2e9b744abe3c91ec825c75e
Guillaume Denry <guillaume.denry@gmail.com>
2016-10-28 16:38:20 +0200

    Relayouting

--- web/elm/src/View.elm
+++ web/elm/src/View.elm
@@ -8,12 +8,13 @@ import Material.Button as Button
 import Material.Tabs as Tabs
 import Material.Icon as Icon
 import Material.Layout as Layout
-import Material.Options as Options exposing (css)
+import Material.Options as Options exposing (css, when)
 import Material.Color as Color
 import Model exposing (Model)
 import Msg exposing (Msg(..))
 import View.Formations
 import View.Formations.New
+import View.Helpers as Helpers
 import Route exposing (Location(..))
 
 
@@ -60,6 +61,62 @@ routeToTab model =
             2
 
 
+type alias MenuItem =
+    { text : String
+    , iconName : String
+    , route : Maybe Route.Location
+    }
+
+
+menuItems : List MenuItem
+menuItems =
+    [ { text = "Formations", iconName = "group", route = Just Formations }
+    , { text = "Students", iconName = "group", route = Just Users }
+    , { text = "Workshops", iconName = "alarm", route = Nothing }
+    ]
+
+
+drawer : Model -> List (Html Msg)
+drawer model =
+    [ Layout.title []
+        [ text "ALICE" ]
+    , Layout.navigation
+        [ Options.css "flex-grow" "1" ]
+        (List.map (drawerMenuItem model) menuItems)
+    ]
+
+
+drawerMenuItem : Model -> MenuItem -> Html Msg
+drawerMenuItem model menuItem =
+    Layout.link
+        [ Layout.onClick (NavigateTo menuItem.route)
+        , (Color.text <| Color.accent) `when` (model.route == menuItem.route)
+        , Options.css "font-weight" "500"
+        , Options.css "cursor" "pointer"
+          -- http://outlinenone.com/ TODO: tl;dr don't do this
+          -- Should be using ":focus { outline: 0 }" for this but can't do that with inline styles so this is a hack til I get a proper stylesheet on here.
+        , Options.css "outline" "none"
+        ]
+        [ Icon.view menuItem.iconName
+            [ Options.css "margin-right" "32px"
+            ]
+        , text menuItem.text
+        ]
+
+
+header : Model -> List (Html Msg)
+header model =
+    case model.route of
+        Just Formations ->
+            View.Formations.header model
+
+        Nothing ->
+            Helpers.defaultHeader model "ALICE"
+
+        _ ->
+            View.Formations.header model
+
+
 view : Model -> Html Msg
 view model =
     Layout.render Mdl
@@ -67,16 +124,11 @@ view model =
         [ Layout.selectedTab <| routeToTab model
         , Layout.onSelectTab SelectTab
         , Layout.fixedHeader
+        , Layout.fixedDrawer
         ]
-        { header = [ Layout.row [] [ Layout.title [] [ text "ALICE" ] ] ]
-        , drawer = []
-        , tabs =
-            ( [ div [ id "formations" ] [ text "formations" ]
-              , div [ id "students" ] [ text "Students" ]
-              , div [ id "another" ] [ text "Another thing" ]
-              ]
-            , [ Color.background (Color.color Color.Teal Color.S400) ]
-            )
+        { header = header model
+        , drawer = drawer model
+        , tabs = ( [], [] )
         , main = [ getContent model ]
         }
         |> Material.Scheme.topWithScheme Color.Teal Color.Red
--- web/elm/src/View/Formations.elm
+++ web/elm/src/View/Formations.elm
@@ -1,4 +1,4 @@
-module View.Formations exposing (view)
+module View.Formations exposing (view, header)
 
 import Html exposing (..)
 import Html.Attributes exposing (style)
@@ -11,22 +11,14 @@ import Material.Table as Table
 import Material.Button as Button
 import Material.Icon as Icon
 import Material.Options as Options
+import View.Helpers as Helpers
 
 
 view : Model -> Html Msg
 view model =
     div
         []
-        [ Button.render Mdl
-            [ 0 ]
-            model.mdl
-            [ Button.raised
-            , Button.ripple
-            , Button.onClick <| NavigateTo <| Just NewFormation
-            , Options.css "margin" "5px 5px"
-            ]
-            [ text "New formation" ]
-        , case model.formations of
+        [ case model.formations of
             Nothing ->
                 noFormationsView
 
@@ -45,7 +37,7 @@ noFormationsView =
         [ text "<No formations>" ]
 
 
-formationsView : Model ->  List Formation -> Html Msg
+formationsView : Model -> List Formation -> Html Msg
 formationsView model formations =
     Table.table
         [ Options.css "margin" "5px 5px"
@@ -73,6 +65,7 @@ formationsView model formations =
             )
         ]
 
+
 deleteButton : Model -> Int -> Formation -> Html Msg
 deleteButton model index formation =
     Button.render Mdl
@@ -99,6 +92,36 @@ editButton model index user =
                 [ Button.minifab
                 , Button.colored
                 , Button.ripple
---                , Button.onClick <| NavigateTo <| Just <| EditFormation id
+                  --                , Button.onClick <| NavigateTo <| Just <| EditFormation id
                 ]
                 [ Icon.i "edit" ]
+
+
+addFormationButton : Model -> Html Msg
+addFormationButton model =
+    Button.render Mdl
+        [ 0, 0 ]
+        model.mdl
+        [ Options.css "position" "fixed"
+        , Options.css "display" "block"
+        , Options.css "right" "0"
+        , Options.css "top" "0"
+        , Options.css "margin-right" "35px"
+        , Options.css "margin-top" "35px"
+        , Options.css "z-index" "900"
+        , Button.fab
+        , Button.colored
+        , Button.ripple
+        , Button.onClick <| NavigateTo <| Just NewFormation
+        ]
+        [ Icon.i "add" ]
+
+
+header : Model -> List (Html Msg)
+header model =
+    Helpers.defaultHeaderWithNavigation model
+        "Formations"
+        [ addFormationButton model
+          --  switchViewButton model
+          -- , addUserButton model
+        ]
